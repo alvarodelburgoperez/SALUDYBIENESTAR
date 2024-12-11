aee52c0e0f97756be2559ffe3df0ec67807ad678
Álvaro <114286823+alvarodelburgoperez@users.noreply.github.com>
2024-12-11 19:29:30 +0100

    Add files via upload

--- docker/init.sql
+++ docker/init.sql
@@ -3,26 +3,26 @@ CREATE TABLE clientes (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    phone VARCHAR(20)  -- Permite más caracteres para el teléfono
+    phone VARCHAR(20)
 );
 
 CREATE TABLE proveedores (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    phone VARCHAR(15)
+    phone VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE productos (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL
+    price DECIMAL(10, 2) NOT NULL CHECK(price > 0),
+    stock INT NOT NULL CHECK(stock >= 0)
 );
 
 CREATE TABLE ofertas (
     id SERIAL PRIMARY KEY,
-    product_id INT REFERENCES productos(id),
+    product_id INT REFERENCES productos(id) ON DELETE CASCADE,
     discount DECIMAL(5, 2),
     start_date DATE,
     end_date DATE
@@ -32,5 +32,6 @@ CREATE TABLE mensajes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    message VARCHAR(500) NOT NULL
+    message VARCHAR(500) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
